122fac9624794c67679eea86afa54c31519af7e6
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-02-16 12:49:13 +0100

    categoriemodel class et ses fonctions - à refactoriser

---
+++ Models/CategorieModels.php
@@ -0,0 +1,27 @@
+<?php
+
+class CategorieModels extends Model {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function selectAllFrom()
+    {
+        // inner join
+        $selectAllFrom = $this->pdo->prepare("SELECT products.nom, products.prix, categorie.nom FROM `categorie` INNER JOIN products ON id_categorie=categorie.id");
+        $selectAllFrom -> execute();
+        $resultAllFrom = $selectAllFrom->fetchAll();
+        return $resultAllFrom;
+
+    }
+    //pour sélectionner tous les produits de cette catégorie
+
+    public function addOne($nom)
+    {
+        $addOne=$this->pdo -> prepare('INSERT INTO categorie SET nom=:nom');
+        $addOne -> execute(['nom' => $nom]);
+    }
+}
+
+?>
