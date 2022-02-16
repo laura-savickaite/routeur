97080ece35e3e9c0669cd0a1a20644e757bde435
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-02-16 11:04:30 +0100

    on a remis à jour l'autoloader

--- autoloader.php
+++ autoloader.php
@@ -3,13 +3,19 @@
 spl_autoload_register(function ($class){
     //moi je veux que mon classname soit ControllerAccueil
     // $class = str_replace("\\", "/", $class);
-    var_dump($class);
+    // var_dump($class);
+    // var_dump("./classes/".$class.".php");
     
-    if (file_exists("./Controller".$class.".php")){
+    if (file_exists("./Controller/".$class.".php")){
         require_once("./Controller/".$class.".php");
-    } else if (file_exists($class.".php")) {
-        require_once($class.".php");
+    } 
+    if (file_exists("./Models/".$class.".php")) {
+        require_once("./Models/".$class.".php");
     }
+    if (file_exists("./classes/".$class.".php")) {
+        require_once("./classes/".$class.".php");
+    }
+
 });
 
 
--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php
 
-spl_autoload_register(function ($class){
-    //moi je veux que mon classname soit ControllerAccueil
-    // $class = str_replace("\\", "/", $class);
-    // var_dump($class);
-    // var_dump("./classes/".$class.".php");
-    
-    if (file_exists("./Controller/".$class.".php")){
-        require_once("./Controller/".$class.".php");
-    } 
-    if (file_exists("./Models/".$class.".php")) {
-        require_once("./Models/".$class.".php");
-    }
-    if (file_exists("./classes/".$class.".php")) {
-        require_once("./classes/".$class.".php");
-    }
-
-});
+require_once('autoloader.php');
 
 
 // var_dump($_GET['p']);
